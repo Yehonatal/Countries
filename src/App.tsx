8d9b486c766eadbabf427f81f4908a02ee23013d
yonatan afewerk <yonatanafewerk@gmail.com>
2023-11-16 14:12:28 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <h1 className="font-extrabold m-10 p-2 w-min bg-[#000]">
+                COUNTRIES, BUILDING ...
+            </h1>
+        </>
+    );
+}
+
+export default App;
